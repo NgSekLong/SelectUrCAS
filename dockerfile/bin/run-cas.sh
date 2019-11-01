3a15dfcf08273b36c464bd478a9a5e15eb4ebe01
NgSekLong <longsek@gmail.com>
2019-11-01 12:34:37 +0800

    Add readme to json whitelist

--- dockerfile/bin/run-cas.sh
+++ dockerfile/bin/run-cas.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
 
+# Build cas.properties
+rm /etc/cas/config/cas.properties
+cat /etc/cas/config/*.properties > /etc/cas/config/cas.properties
+
+# Reconstruct build.gradle
+cp build.default.gradle build.gradle
+echo "dependencies {"  >> build.gradle
+cat ./build-gradle-dependencies/*.gradle >> build.gradle
+echo "}"  >> build.gradle
+
+
 
 ./gradlew clean build --parallel;
 cp ./build/libs/cas.war cas.war
---
+++ source/authentication/json-whitelist/README.md
@@ -0,0 +1,8 @@
+## JSON Whitelist Authentication
+
+Documentation: https://apereo.github.io/cas/6.1.x/installation/Whitelist-Authentication.html
+
+Login using:
+
+- Username: madlad
+- Password: Mellon
