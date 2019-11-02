abb926969f9dcfc8bf65a3b6133bdde9981af0e5
NgSekLong <longsek@gmail.com>
2019-11-02 15:01:59 +0800

    Add cas-overlay

---
+++ source/cas-overlay/bin/run-cas.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+
+# Build cas.properties
+rm -f /etc/cas/config/cas.properties
+find /etc/cas/config -type f -name '*.properties' -exec cat {} + > /etc/cas/config/cas.properties
+#cat /etc/cas/config/*.properties > /etc/cas/config/cas.properties
+
+# Reconstruct build.gradle
+cp build.default.gradle build.gradle
+echo "dependencies {"  >> build.gradle
+
+find ./build-gradle-dependencies/ -type f -name '*.properties' -exec cat {} + >> build.gradle
+#cat ./build-gradle-dependencies/*.gradle >> build.gradle
+echo "}"  >> build.gradle
+
+
+# Build CAS
+./gradlew clean build --parallel;
+cp ./build/libs/cas.war cas.war
+
+# Attempt to trigger the first login, so subsequent login is faster
+sleep 60 && curl -k https://127.0.0.1:8443/cas/login &
+
+exec java -server -noverify -Xmx2048M -jar cas.war
+
