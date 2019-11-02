5782b8fcec14bd172c07112e565db26aaf2c225d
NgSekLong <longsek@gmail.com>
2019-11-02 16:21:01 +0800

    fix mysql after using constant variable

--- source/cas-overlay/bin/run-cas.sh
+++ source/cas-overlay/bin/run-cas.sh
@@ -12,9 +12,7 @@ find /etc/cas/config/properties -type f -name '*.yml' -exec cat {} + > /etc/cas/
 # Reconstruct build.gradle
 cp build.default.gradle build.gradle
 echo "dependencies {"  >> build.gradle
-
-find ./build-gradle-dependencies/ -type f -name '*.properties' -exec cat {} + >> build.gradle
-#cat ./build-gradle-dependencies/*.gradle >> build.gradle
+find ./build-gradle-dependencies/ -type f -name 'build.gradle' -exec cat {} + >> build.gradle
 echo "}"  >> build.gradle
 
 
@@ -23,7 +21,10 @@ echo "}"  >> build.gradle
 cp ./build/libs/cas.war cas.war
 
 # Attempt to trigger the first login, so subsequent login is faster
-sleep 60 && curl -k https://127.0.0.1:8443/cas/login &
+for i in 20 30 40 50 60 70 80 90 100
+do
+sleep $i && curl -s -k 'https://127.0.0.1:8443/cas/login' > /dev/null &
+done
 
 exec java -server -noverify -Xmx2048M -jar cas.war
 
