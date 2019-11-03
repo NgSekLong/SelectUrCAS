09eceaa0e3e325c99bba8fb60d15c490eed150ea
NgSekLong <longsek@gmail.com>
2019-11-03 17:13:30 +0800

    Make the refernece website fully functional

--- bin/convert_path_to_env.sh
+++ bin/convert_path_to_env.sh
@@ -15,8 +15,13 @@ cp $env_default_file $env_file
 echo "########Avaliable variable###########"
 for dir in `find . -type d`
 do
-
+    # Don't root
     if [ "$dir" == "." ]; then
+        continue
+    fi
+
+    # If directory don't have docker-compose.yml, don't make variable
+    if [ ! -f "$dir/docker-compose.yml" ]; then
         continue
     fi
 
@@ -24,19 +29,29 @@ do
     constant=${constant//\.\//}
     constant=${constant//\//_}
 
+    # Set Constant Value
     constant_name=${constant//_/-}
-    echo $constant_name
     constant=$(echo $constant | tr a-z A-Z)
+    type=`echo $dir | sed 's/\.\/\(.*\)\/.*/\1/'`
+    path="./source/${dir:2}"
 
+
+    # Set Constant name
     constant=${constant//-/_}
     constant_path="$constant"_PATH
+    constant_type="$constant"_TYPE
 
     # Write to .env
     echo $constant=$constant_name >> $env_file
-    echo $constant_path=./source/$dir >> $env_file
 
+    echo $constant_path=$path >> $env_file
+    echo $constant_type=$type >> $env_file
+
+
+    # Output for better copy and paste
     echo 
-    echo "\${$constant_path:-.} => ./source/$dir"
+    echo "\${$constant_type:-.} => $type"
+    echo "\${$constant_path:-.} => $path"
     echo "\${$constant} => $constant_name"
 
 done
