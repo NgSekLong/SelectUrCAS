d343779e5d55bba5a8795ca5510ef422766781df
Ng Sek Long <longsek@gmail.com>
2021-06-30 13:52:07 +0000

    Fix OAuth 2 PHP client

--- source/other-client/oauth2-php-client/README.md
+++ source/other-client/oauth2-php-client/README.md
@@ -2,3 +2,5 @@
 
 Access client by:
 - https://cas.example.org:57575
+
+Documentation: https://github.com/thephpleague/oauth2-client
--- source/other-client/oauth2-php-client/index.php
+++ source/other-client/oauth2-php-client/index.php
@@ -80,7 +80,7 @@ if (!isset($_GET['code'])) {
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Phpcas Login Example</title>
+<title>OAuth2 Client Login Example</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
