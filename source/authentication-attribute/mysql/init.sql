c5cd707471d410ca7d515292046c58d891a0ee10
Ng Sek Long <longsek@gmail.com>
2021-07-01 07:55:20 +0000

    Add user to mysql authentication attribute

--- source/authentication-attribute/mysql/init.sql
+++ source/authentication-attribute/mysql/init.sql
@@ -14,3 +14,10 @@ VALUES
   ('casuser', 'firstname', 'Cool'),
   ('casuser', 'lastname', 'Dude'),
   ('casuser', 'phone', '+13476452319');
+
+INSERT INTO USERATTRS (uid,  attrname, attrvalue) VALUES ('freeradius', 'attributeRetriveFrom', 'mysql'), ('freeradius', 'jsonUsernameRetrived', 'freeradius');
+INSERT INTO USERATTRS (uid,  attrname, attrvalue) VALUES ('json-whitelist', 'attributeRetriveFrom', 'mysql'), ('json-whitelist', 'jsonUsernameRetrived', 'json-whitelist');
+INSERT INTO USERATTRS (uid,  attrname, attrvalue) VALUES ('mongodb', 'attributeRetriveFrom', 'mysql'), ('mongodb', 'jsonUsernameRetrived', 'mongodb');
+INSERT INTO USERATTRS (uid,  attrname, attrvalue) VALUES ('mysql-query', 'attributeRetriveFrom', 'mysql'), ('mysql-query', 'jsonUsernameRetrived', 'mysql-query');
+INSERT INTO USERATTRS (uid,  attrname, attrvalue) VALUES ('openldap', 'attributeRetriveFrom', 'mysql'), ('openldap', 'jsonUsernameRetrived', 'openldap');
+INSERT INTO USERATTRS (uid,  attrname, attrvalue) VALUES ('rest', 'attributeRetriveFrom', 'mysql'), ('rest', 'jsonUsernameRetrived', 'rest');
