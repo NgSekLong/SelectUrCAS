a4d2ec09a1f471d72c277699f8b359729640bfa0
Ng Sek Long <longsek@gmail.com>
2021-07-03 21:55:02 +0800

    Code refractor

--- source/cas-client/phpcas/index.php
+++ source/cas-client/phpcas/index.php
@@ -2,11 +2,7 @@
 require __DIR__ . '/vendor/autoload.php';
 phpCAS::client(CAS_VERSION_3_0,$_ENV["CAS_DOMAIN_NAME"],8443,"/cas/");
 
-// This setting (i.e. setNoCasServerValidation) is only for testing, not for production 
-// phpCAS::setNoCasServerValidation();
-
-//phpCAS::setCasServerCACert("./CACert.pem");//this is relative to the cas client.php file
-phpCAS::setCasServerCACert("/etc/cert.crt",false);//this is relative to the cas client.php file
+phpCAS::setCasServerCACert("/etc/cert.crt",false);
 
 $isAuthenticated = false;
 if (phpCAS::isAuthenticated())
--- source/cas-client/phpcas/logout.php
+++ source/cas-client/phpcas/logout.php
@@ -4,11 +4,8 @@ require __DIR__ . '/vendor/autoload.php';
 
 phpCAS::client(CAS_VERSION_3_0,$_ENV["CAS_DOMAIN_NAME"],8443,"/cas/");
 
-// This setting (i.e. setNoCasServerValidation) is only for testing, not for production 
-// phpCAS::setNoCasServerValidation();
-
-//phpCAS::setCasServerCACert("./CACert.pem");//this is relative to the cas client.php file
 phpCAS::setCasServerCACert("/etc/cert.crt");
+
 if (phpCAS::isAuthenticated())
 {
     phpCAS::logout();
